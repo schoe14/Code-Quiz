8a4390ce92dd6bfc34b8392ee178299846c58f48
Seohui Choe <chelsea.choe@gmail.com>
2019-11-28 02:16:06 -0500

    have a mvp

--- questions.js
+++ questions.js
@@ -1,12 +1,88 @@
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
+        title: "The unordered collection of properties, each of which has a name and a value is called",
+        choices: ["String", "Object", "Serialized Object", "All of the mentioned"],
+        answer: "Object"
     },
     {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "JavaScript can be written",
+        choices: ["Directly into JS file and included into HTML", 
+        "Directly on the server page", "Directly into HTML pages", "All of the mentioned"],
+        answer: "Directly into JS file and included into HTML"
     },
+    {
+        title: "A proper scripting language is a",
+        choices: ["Low level programming language", "High level programming language", 
+        "Assembly level programming language", "Machine level programming language"],
+        answer: "High level programming language"
+    },
+    {
+        title: "JavaScript Code can be called by using",
+        choices: ["RMI", "Triggering Event", "Preprocessor", "Function/Method"],
+        answer: "Function/Method"
+    },
+    {
+        title: "The statement a===b refers to",
+        choices: ["Both a and b are equal in value, type and reference address", 
+        "Both a and b are equal in value", "Both a and b are equal in value and type", "There is no such statement"],
+        answer: "Both a and b are equal in value and type"
+    }  
 ];
+
+
+var questions2 = [
+    {
+        title: "HTML stands for?",
+        choices: ["Hyper Text Markup Language", "High Text Markup Language", "Hyper Tabular Markup Language", "None of these"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        title: "Which of the following tag is used to mark a begining of paragraph?",
+        choices: ["<TD>", "<br>", "<P>", "<TR>"],
+        answer: "<P>"
+    },
+    {
+        title: "The attribute of <form> tag",
+        choices: ["Method", "Action", "Both (a)&(b)", "None of these"],
+        answer: "Both (a)&(b)"
+    },
+    {
+        title: "Markup tags tell the web browser",
+        choices: ["How to organise the page", "How to display the page", "How to display message box on page", "None of these"],
+        answer: "How to display the page"
+    },
+    {
+        title: "What are Empty elements and is it valid?",
+        choices: ["No, there is no such terms as Empty Element", "Empty elements are element with no data", 
+        "No, it is not valid to use Empty Element", "None of these"],
+        answer: "Empty elements are element with no data"
+    }
+];
+
+var questions3 = [
+    {
+        title: "What does CSS stand for?",
+        choices: ["Creative Style Sheets", "Colorful Style Sheets", "Cascading Style Sheets", "Computer Style Sheets"],
+        answer: "Cascading Style Sheets"
+    },
+    {
+        title: "Where in an HTML document is the correct place to refer to an external style sheet?",
+        choices: ["At the end of the document", "In the <head> section", "At the top of the document", "In the <body> section"],
+        answer: "In the <head> section"
+    },
+    {
+        title: "Which is the correct CSS syntax?",
+        choices: ["body {color: black}", "{body;color:black}", "{body:color=black(body}", "body:color=black"],
+        answer: "body {color: black}"
+    },
+    {
+        title: "How do you change the text color of an element?",
+        choices: ["text-color=", "fgcolor:", "color:", "text-color:"],
+        answer: "color:"
+    },
+    {
+        title: "How do you display hyperlinks without an underline?",
+        choices: ["a {text-decoration:no underline}", "a {decoration:no underline}", "a {text-decoration:none}", "a {underline:none}"],
+        answer: "a {text-decoration:none}"
+    }
+]
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 var navLinkEl = document.getElementById("navLink");
 var timeValueEl = document.getElementById("timeValue");
+
 var firstPageEl = document.getElementById("firstPage");
 var startBtn = document.getElementById("startBtn");
 
 var currentScorePageEl = document.getElementById("currentScorePage");
 var currentScoreEl = document.getElementById("currentScore");
+var userInputFormEl = document.getElementById("userInput-form");
 var userInputEl = document.getElementById("userInput");
 var submitBtn = document.getElementById("submitBtn");
 
@@ -14,20 +16,28 @@ var goBackBtn = document.getElementById("goBackBtn");
 var clearBtn = document.getElementById("clearBtn");
 
 var quizPageEl = document.getElementById("quizPage");
+var question = document.getElementById("question");
+var choicesGroup = document.getElementById("choicesGroup");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var checkAns = document.getElementById("checkAns");
 
+var timeInterval;
 var timeLeft;
 
 // If View Highscores nav link is clicked, go to displayHighscores()
 navLinkEl.addEventListener("click", function () {
-    event.preventDefault();
+    clearInterval(timeInterval);
     displayHighscores();
 })
 
-// Displays intro page when loaded
+// Displays the first page when loaded
 function firstPageFunction() {
     firstPageEl.style.display = "block";
     highscoresPageEl.style.display = "none";
-    timeLeft = 5;
+    timeLeft = 75;
     timeValueEl.innerHTML = "Time: " + timeLeft;
 }
 
@@ -40,92 +50,126 @@ startBtn.addEventListener("click", function (event) {
 });
 
 function timer() {
-    var timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
         timeLeft--;
-        timeValueEl.textContent = "Time: " + timeLeft;
-
-        // While time > 0, displays questions (randomly sorted)
-        // If wrong answer, subtract 15 sec from the remaining time
 
         // If time runs out or no questions left, go to next function (displayScore())
-        if (timeLeft === 0 || runningQuestion > lastQuestion) {
+        if (timeLeft <= 0) {
+            timeLeft = 0;
             clearInterval(timeInterval);
-            quizPageEl.style.display = "none";
             displayScore();
         }
-        // console.log(timeLeft);
+    
+        if (runningQuestion > lastQuestion) {
+            clearInterval(timeInterval);
+            displayScore();
+        }
+        displayTime(); 
 
     }, 1000);
 }
 
-var question = document.getElementById("question");
-var choicesGroup = document.getElementById("choicesGroup");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
-var checkAns = document.getElementById("checkAns");
+function displayTime() {
+    timeValueEl.textContent = "Time: " + timeLeft;
+}
 
 var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
-var q = questions[runningQuestion];
 
+// While time > 0 or questions left, render questions
 function renderQuestion() {
-    q = questions[runningQuestion];
-    console.log(q);
+
     quizPageEl.style.display = "block";
     firstPageEl.style.display = "none";
-    // questions.forEach((element) => {
-    if (runningQuestion > lastQuestion) {
-        return timeLeft;
-    }
-    question.innerHTML = q.title;
-    choiceA.innerHTML = q.choices[0];
-    choiceB.innerHTML = q.choices[1];
-    choiceC.innerHTML = q.choices[2];
-    choiceD.innerHTML = q.choices[3];
+    var q = questions[runningQuestion];
 
-    // })
+    if (runningQuestion <= lastQuestion) {
+        question.innerHTML = runningQuestion+1 + ". " + q.title;
+        choiceA.innerHTML = "1. " + q.choices[0];
+        choiceB.innerHTML = "2. " + q.choices[1];
+        choiceC.innerHTML = "3. " + q.choices[2];
+        choiceD.innerHTML = "4. " + q.choices[3];
+    }
+
 }
 
-function checkAnswer(userPick) {
+// While users get to choose answer, check if the answer is correct
+// If answer is wrong, subtract 15 sec from the remaining time
+choicesGroup.addEventListener("click", function (event) {
     event.preventDefault();
+    let element = event.target.innerHTML;
+    console.log(element);
     checkAns.style.display = "block";
-    if (userPick == questions[runningQuestion].answer) checkAns.innerHTML = "Correct!";
-    else { checkAns.innerHTML = "Wrong!"; }
+    if (element.substring(3) == questions[runningQuestion].answer) {
+        checkAns.innerHTML = "Correct!";
+    }
+    else {
+        checkAns.innerHTML = "Wrong!";
+        timeLeft -= 15;
+    }
     runningQuestion++;
     setTimeout(function () {
         $('#checkAns').fadeOut('fast');
-    }, 400);
+    }, 800);
     renderQuestion();
-}
+})
 
 // Displays a page that shows score for this run and takes user initials
 function displayScore() {
     currentScorePageEl.style.display = "block";
+    quizPageEl.style.display = "none";
     currentScoreEl.innerHTML = "Your score is " + timeLeft;
 }
 
-// If submit button is clicked, stores user input & score to local storage and go to next function (displayhighscores())
+// If enter key or submit button is clicked, push user input & score to array and store the array in local storage
+// Finally, go to next function (displayhighscores())
+$('#userInput').keypress(function (e) {
+    var key = e.which;
+    if (key == 13) {
+        submitBtn.click();
+        return false;
+    }
+});
+
+var scoresArr = [];
+
 submitBtn.addEventListener("click", function () {
     event.preventDefault();
-    localStorage.setItem(userInputEl.value, timeLeft);
+    let storedValues = JSON.parse(localStorage.getItem("scores"));
+    let scoreText = userInputEl.value.trim();
+
+    if (scoreText === "") return;
+
+    // Check if there are stored values in local storage
+    if (storedValues !== null) {
+        scoresArr = storedValues;
+    }
+
+    scoresArr.push({ "name": scoreText, "score": timeLeft });
+    scoresArr.sort((a, b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0));
+    userInputEl.value = "";
+
+    localStorage.setItem("scores", JSON.stringify(scoresArr));
     displayHighscores();
 })
 
-// Adds the user input & score to local storage and displays the stored items
+// Retrieves the items in local storage
 function displayHighscores() {
     highscoresPageEl.style.display = "block";
     firstPageEl.style.display = "none";
     currentScorePageEl.style.display = "none";
+    quizPageEl.style.display = "none";
     scoreListEl.innerHTML = "";
 
-    for (var i = 0; i < localStorage.length; i++) {
-        var li = document.createElement("li");
-        var value = localStorage.key(i) + " - " + localStorage.getItem(localStorage.key(i));
-        li.append(document.createTextNode(value));
-        scoreListEl.append(li);
-        console.log(localStorage.key(i));
+    let storedValues = JSON.parse(localStorage.getItem("scores"));
+
+    if (storedValues != null) {
+        for (var i = 0; i < storedValues.length; i++) {
+            var li = document.createElement("li");
+            var value = i + 1 + ".  " + storedValues[i].name + ":  " + storedValues[i].score;
+            li.append(document.createTextNode(value));
+            scoreListEl.append(li);
+        }
     }
 }
 
